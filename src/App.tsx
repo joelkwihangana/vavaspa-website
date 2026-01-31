b99376daea34e6dd8b17e3fc6c83ef2eea7ad9cf
Joel Kwihangana <joelkwijoe.com>
2026-02-01 00:33:37 +0200

    Feat:  design hero and intro with editorial layout and mission overlap

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MassagePage from "./pages/MassagePage";
 import SpaPage from "./pages/SpaPage";
+import AboutPage from "./pages/AboutPage";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/massage" element={<MassagePage />} />
         <Route path="/spa" element={<SpaPage />} />
+        <Route path="/about" element={<AboutPage />} />
       </Routes>
     </BrowserRouter>
   );
