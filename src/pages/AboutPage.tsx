cf57d305fc050a3368cbe27c2d37ccd1d38b3a63
Joel Kwihangana <joelkwijoe.com>
2026-02-01 01:24:24 +0200

    refactor(about): reorder sections for stronger narrative flow and reduced scroll fatigue

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -4,9 +4,9 @@ import Footer from "../components/layout/Footer";
 import AboutHero from "../components/sections/about/AboutHero";
 import AboutIntro from "../components/sections/about/AboutIntro";
 import AboutUnique from "../components/sections/about/AboutUnique";
-import AboutStoryEditorial from "../components/sections/about/AboutStoryEditorial";
 import AboutJourney from "../components/sections/about/AboutJourney";
 import AboutApproachBand from "../components/sections/about/AboutApproachBand";
+import AboutStoryEditorial from "../components/sections/about/AboutStoryEditorial";
 import AboutClosingSplit from "../components/sections/about/AboutClosingSplit";
 import AboutCta from "../components/sections/about/AboutCta";
 
@@ -18,9 +18,9 @@ export default function AboutPage() {
         <AboutHero />
         <AboutIntro />
         <AboutUnique />
-        <AboutStoryEditorial />
         <AboutJourney />
         <AboutApproachBand />
+        <AboutStoryEditorial />
         <AboutClosingSplit />
         <AboutCta />
       </main>
