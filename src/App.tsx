73c880de71e0068ea374dc3bc1b52c56022d74fb
Joel Kwihangana <joelkwijoe.com>
2026-01-31 15:37:35 +0200

    feat: align booking form services with spa menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/layout/Navbar";
 import HeroCarousel from "./components/sections/HeroCarousel";
+import BookingSection from "./components/sections/BookingSection";
 
 export default function App() {
   return (
     <div className="bg-bg text-text">
       <Navbar />
       <HeroCarousel />
+      <BookingSection />
     </div>
   );
 }
