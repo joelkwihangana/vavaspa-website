be9f476d699933cce49a2c306eca4f9b97ded08f
Joel Kwihangana <85849394+joelkwihangana@users.noreply.github.com>
2026-02-02 12:58:08 +0000

    refactor(about): improve page rhythm, add dividers, reduce stacked-section fatigue

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -2,10 +2,8 @@ import AboutHero from "../components/sections/about/AboutHero";
 import AboutIntro from "../components/sections/about/AboutIntro";
 import AboutUnique from "../components/sections/about/AboutUnique";
 import AboutJourney from "../components/sections/about/AboutJourney";
-import AboutApproachBand from "../components/sections/about/AboutApproachBand";
 import AboutStoryEditorial from "../components/sections/about/AboutStoryEditorial";
 import AboutClosingSplit from "../components/sections/about/AboutClosingSplit";
-// import AboutCta from "../components/sections/about/AboutCta";
 import TeamSection from "../components/sections/team/TeamSection";
 
 export default function AboutPage() {
@@ -13,14 +11,40 @@ export default function AboutPage() {
     <div className="bg-bg text-text">
       <main>
         <AboutHero />
-        <AboutIntro />
-        <AboutUnique />
-        <TeamSection />
-        <AboutJourney />
-        <AboutApproachBand />
-        <AboutStoryEditorial />
-        <AboutClosingSplit />
-        {/* <AboutCta /> */}
+
+        <div className="section-tight">
+          <AboutIntro />
+        </div>
+
+        <div className="section-divider" />
+
+        <div className="section-tight">
+          <AboutUnique />
+        </div>
+
+        <div className="section-divider" />
+
+        <div className="section-tight">
+          <TeamSection />
+        </div>
+
+        <div className="section-divider" />
+
+        <div className="section">
+          <AboutJourney />
+        </div>
+
+        <div className="section-divider" />
+
+        <div className="section-tight">
+          <AboutStoryEditorial />
+        </div>
+
+        <div className="section-divider" />
+
+        <div className="section-tight safe-bottom">
+          <AboutClosingSplit />
+        </div>
       </main>
     </div>
   );
